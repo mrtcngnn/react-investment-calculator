6ac16d1bd63dcc11c72ea1628d3bfffc1ae505c8
mrtcngnn <mrtcngonen@gmail.com>
2023-10-28 15:54:35 +0300

    header img alt change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import headerLogo from "../assets/investment-calculator-logo.png";
 export default function Header() {
   return (
     <div id="header">
-      <img src={headerLogo} alt="headerLogo"></img>
+      <img src={headerLogo} alt="header-logo"></img>
       <h1>React Investment Calculator</h1>
     </div>
   );
